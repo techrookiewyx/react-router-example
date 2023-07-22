1f86a7096dab585bd670546a0d25500bd1d970c3
wangyongxin <676425826@qq.com>
2023-07-23 00:00:27 +0800

    懒加载案例-子组件

---
+++ my-app/src/lazy-loading/pages/Dashboard.jsx
@@ -0,0 +1,49 @@
+import { Routes, Route, Outlet, Link } from "react-router-dom";
+export default function Dashboard() { 
+  return (
+      <Routes>
+        <Route path='' element={<DashboardLayout />}>
+          <Route index element={<DashboardIndex />} />
+          <Route path='messages' element={<Messages />} />
+        </Route>
+      </Routes>
+  )
+}
+
+function DashboardLayout(){
+  return(
+    <div>
+      <nav>
+        <ul>
+          <li><Link to=''>Dashboard Home</Link></li>
+          <li>
+            <Link to="messages">Messages</Link>
+          </li>
+        </ul>
+      </nav>
+      <hr />
+      <Outlet/>
+    </div>
+  )
+}
+
+function DashboardIndex() {
+  return (
+    <div>
+      <h2>Dashboard Index</h2>
+    </div>
+  );
+}
+
+
+function Messages() {
+  return (
+    <div>
+      <h2>Messages</h2>
+      <ul>
+        <li>Message 1</li>
+        <li>Message 2</li>
+      </ul>
+    </div>
+  );
+}
