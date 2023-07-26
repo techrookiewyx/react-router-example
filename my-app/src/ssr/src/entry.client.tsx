d849ca8d8a6010ea29f85c34069df8c35eece6d9
wangyongxin <676425826@qq.com>
2023-07-27 00:04:16 +0800

    服务端渲染案例-客户端部分

---
+++ my-app/src/ssr/src/entry.client.tsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+
+import App from "./App";
+
+ReactDOM.hydrateRoot(
+  document.getElementById("app")!,
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
